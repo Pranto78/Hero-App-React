ab64ff937ddedd837bda605c862b7a410a0c33f3
Pranto78 <fshahriyar44@gmail.com>
2025-10-08 15:18:17 +0600

    Added the banner number one

---
+++ src/Components/BannerOne/BannerOne.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import gImg from '../../assets/fi_16076057.png';
+import storeImg from '../../assets/Group.png';
+import heroImg from '../../assets/hero.png';
+
+const BannerOne = () => {
+    return (
+        <div className='text-center max-w-[1200px] mx-auto p-6'>
+                <h1 className='text-4xl font-bold'>We Build <br />
+                <span className='text-[#9F62F2]'>Productive</span> Apps
+                </h1>
+
+                <p className='text-gray-600 mt-3'>At HERO.IO , we craft innovative apps designed to make everyday life simpler, smarter, and more exciting.Our goal is to turn your ideas into digital experiences that truly make an impact.</p>
+
+                <div className='flex justify-center gap-3 mt-3'>
+                    <button className="btn p-2"><img src={gImg } alt="" /> Google Play</button>
+                    <button className="btn p-2"><img src={storeImg} alt="" /> App Store</button>
+
+                </div>
+
+                <div className='flex justify-center mt-4'>
+                    <img src={heroImg} alt="" />
+                </div>
+        </div>
+    );
+};
+
+export default BannerOne;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import BannerOne from '../BannerOne/BannerOne';
 
 const Home = () => {
     return (
         <div>
-            <h2>Home</h2>
+            <BannerOne></BannerOne>
         </div>
     );
 };
